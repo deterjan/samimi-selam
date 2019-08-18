b7232e3ef2f4e0110993448beec5e331deb40e0e
Ulkudas <eneskeles81@gmail.com>
2019-08-18 16:57:25 +0300

    added basic ileri geri

--- samimi.js
+++ samimi.js
@@ -44,6 +44,70 @@ function makeGreeting() {
 	let greetingWords = timedGreetingWords(new Date().getHours());
 	let adjective = pickRandom(turkishAdjectives);
 	let noun = makePlural(clearSuffix(pickRandom(turkishNouns)));
+	let greeting = (greetingWords + " " + adjective + " " + noun); 
+	addGreeting(greeting);
+	return greeting;
+}
 
-	return (greetingWords + " " + adjective + " " + noun);
+function GreetingQueue(size, index, greetings) {
+	this.size = size;
+	this.index = index;
+	this.greetings = greetings;
+}
+
+function loadGreetingQueue() {
+	if ("size" in sessionStorage) {
+		let size = parseInt(sessionStorage.getItem("size"), 10);
+		let index = parseInt(sessionStorage.getItem("index"), 10);
+		let greetings = JSON.parse(sessionStorage.getItem("greetings"));
+		return new GreetingQueue(size, index, greetings);
+	}
+	return new GreetingQueue(0, -1, []);
+}
+
+function saveGreetingQueue(queue) {
+	sessionStorage.setItem("size", queue.size.toString());
+	sessionStorage.setItem("index", queue.index.toString());
+	sessionStorage.setItem("greetings", JSON.stringify(queue.greetings));
+}
+
+function hasNextGreeting(queue) {
+	return (queue.index < (queue.size - 1));
+}
+
+function hasPreviousGreeting(queue) {
+	return (queue.index > 0);
+}
+
+function addGreeting(greeting) {
+	let queue = loadGreetingQueue();
+	queue.greetings = queue.greetings.slice(0, queue.index + 1)
+	queue.greetings.push(greeting);
+	queue.size = queue.greetings.length;
+	queue.index = queue.size - 1;
+	saveGreetingQueue(queue);
+}
+
+function getPreviousGreeting() {
+	let queue = loadGreetingQueue();
+
+	if (hasPreviousGreeting(queue)) {
+		let prevGreeting = queue.greetings[queue.index - 1];
+		queue.index--;
+		saveGreetingQueue(queue);
+		return prevGreeting;
+	}
+	else return null;
+}
+
+function getNextGreeting() { 
+	let queue = loadGreetingQueue();
+
+	if (hasNextGreeting(queue)) {
+		let nextGreeting = queue.greetings[queue.index + 1];
+		queue.index++;
+		saveGreetingQueue(queue);
+		return nextGreeting;
+	}
+	else return null;
 }
